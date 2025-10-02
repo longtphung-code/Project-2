77a9c854e23b913580689800d40decfed7aa5b7e
longtphung-code <lphung15433@email.vccs.edu>
2025-10-01 22:55:52 -0400

    Distance per tank of gas

--- Project 2/Project 2.cpp
+++ Project 2/Project 2.cpp
@@ -4,13 +4,15 @@
 
 int main()
 {
-	// Algorithimic steps; 1 Create a variable for the the MPG when a car travels in town and on the highway
-	// step 2 multiply each variable by 20 gallons to get the distance the car can travel on one tank of gas for each respective variable
-	// step 3 print the results to the console.
-    double a = 23.5; // a represent the mile per gallon when driven in town
-	double b = 28.9; // b represents the mile per gallon when driven on the highwya
-    double Milestraveledintown = a * 20; // this line calculates the distance the car can travel on one tank of gas in town
-	double Milestraveledonthehighway = b * 20; // this line calculates the distance the car can travel on one tank of gas on the highwya
+	// Algorithimic steps; Create a value to represent total number of surveyed customers
+	// step 2 Create a value to represent the percentage of  the surveyed customers that purchased one or more energy drinks per week
+	// step 3 multiply value from step 1 by step 3, than multiply this value by 58%. The number produced is the final answer
+    double a = 16500; // A represents the number of people surveyed by a soft drink company.
+	double b = 0.15; // B represents the percentage of people who purcahsed one or more energy drinks per week.
+	double c = a * b; // This line calculates the number of people who purchase one or more energy drinks per week.
+	double d = 0.58; // D represents the percent of people who prefer citrus flavored energy drinks.
+	double e = c * d; // This line calculates the number of people who prefer citrus flavored energy drinks.
+
   
 	// this line prints the results to the console
 	std::cout << "A car has a gas mileage of " << a << " miles per gallon in town" << std::endl;
