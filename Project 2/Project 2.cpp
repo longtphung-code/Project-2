a760dd1a2cc060ed584c277674d086333e715e7a
longtphung-code <lphung15433@email.vccs.edu>
2025-10-01 20:10:29 -0400

    Distance per tank of gas project

--- Project 2/Project 2.cpp
+++ Project 2/Project 2.cpp
@@ -4,5 +4,18 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	// Algorithimic steps; 1 Create a variable for the the MPG when a car travels in town and on the highway
+	// step 2 multiply each variable by 20 gallons to get the distance the car can travel on one tank of gas for each respective variable
+	// step 3 print the results to the console.
+    double a = 23.5; // a represent the mile per gallon when driven in town
+	double b = 28.9; // b represents the mile per gallon when driven on the highwya
+    double Milestraveledintown = a * 20; // this line calculates the distance the car can travel on one tank of gas in town
+	double Milestraveledonthehighway = b * 20; // this line calculates the distance the car can travel on one tank of gas on the highwya
+  
+	// this line prints the results to the console
+	std::cout << "A car has a gas mileage of " << a << " miles per gallon in town" << std::endl;
+	std::cout << "A car has a gas mileage of " << b << " miles per gallon on the highway " << std::endl;
+	std::cout << "When driven in town the car can travel " << Milestraveledintown << " miles on one tank" << std::endl;
+	std::cout << "When driven on the highway the car can travel " << Milestraveledonthehighway << " miles on one tank" << std::endl;
+	return 0;
 }
